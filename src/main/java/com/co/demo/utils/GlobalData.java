648d208a1a5d7114210cba794e91864d8624a057
Yvan Fabrizzio Ranilla Marquez <yranillam@choucairtesting.com>
2024-07-05 17:25:30 +0000

    Deleted src

--- src/main/java/com/co/demo/utils/GlobalData.java
+++
@@ -1,9 +0,0 @@
-package com.co.demo.utils;
-
-import freemarker.core.Environment;
-import net.thucydides.core.util.EnvironmentVariables;
-
-public class GlobalData {
-    public static EnvironmentVariables environmentVariables;
-    public static String baseUrl = "https://demo.serenity.is/Account/Login/?ReturnUrl=%2F";
-}
