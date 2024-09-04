c7b8be9e07022d84fcf8c79ba963aa0e0b0374bb
yranillam <yranillam@choucairtesting.com>
2024-09-04 12:47:51 -0500

    prueba

--- Prueba.java
+++ Prueba.java
@@ -2,16 +2,7 @@ public class Prueba {
 
     private PageCambioLeadYOportu pageCambioLeadYOportu;
 
-    @Step
-    public void desestimarLead(String strMotivo, String strDetalle, String strResulCelular) {
-        pageCambioLeadYOportu.irDesestimar();
-        pageCambioLeadYOportu.ingresarDatosYDesestimar(strMotivo, strDetalle, strResulCelular);
-    }
+    Prueba
 
-    @Step
-    public String obtenerEstadoProspecto() {
-        return pageCambioLeadYOportu.extraerEstadoProspecto();
-    }
 
-    
 }
